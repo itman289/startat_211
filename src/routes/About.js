e7618ca7d01629e256b732c6c13d0d408fdb8bad
itman289 <kghie354@gmail.com>
2021-05-09 01:30:18 +0900

    gogo

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About(props){
+    console.log(props);
+    return (
+    <div className="about_container">
+        <span> 
+            HI. THIS IS ABOUT PAGE 
+        </span>
+    </div>
+    );
+}
+
+export default About;
